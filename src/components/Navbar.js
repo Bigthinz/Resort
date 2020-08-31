cadd24e298b924b63fa0416f5a7fd4f9f7e720bd
bigthinz <bigthinzcount@gmail.com>
2020-08-31 16:06:45 +0000

    react init

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                hey here is our navbar!!!!
+            </div>
+        )
+    }
+}
