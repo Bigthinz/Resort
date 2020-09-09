e4500a7a9e2ff3fe1a8be161a02cf3f753e6f53c
bigthinz <bigthinzcount@gmail.com>
2020-09-09 15:04:22 +0000

    single-room cpt

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -31,7 +31,7 @@ export default class SingleRoom extends Component {
             </div>  
         }
 
-        const {name,description, capacity, size , price, extras, breakfast, pest, images} = room
+        const {name,description, capacity, size , price, extras, breakfast, pet, images} = room
 
         const domImg =images.slice(1,4)
 
@@ -49,6 +49,36 @@ export default class SingleRoom extends Component {
                     <img key={index} src={image} alt={name}/>
                     ))}
                 </div>
+
+                <div className='single-room-info'>
+                    <article className='desc'>
+                        <h3>details</h3>
+                        <p>{description}</p>
+
+                    </article>
+                    <article className='info'>
+                        <h3>Info</h3>
+                        <h6>price: ${price}</h6>
+                        <h6>size: {size} SQFT</h6>
+                        <h6>max capacity: {""}
+                            {capacity > 1? `${capacity} people`: `${capacity} person`}
+                        </h6>
+                        <h6>{pet? "Pets allowed": "Pet not allowed"}</h6>
+                        <h6>{breakfast && "free breakfast included"}</h6>
+
+                    </article>
+
+                </div>
+
+            </section>
+
+            <section className='room-extras'>
+                <h6>Extras</h6>  
+                <ul className='extras'>
+                        {extras.map((item,index)=>{
+                            return <li key={index}>- {item}</li>
+                        })}
+                </ul>
 
             </section>
             </>
